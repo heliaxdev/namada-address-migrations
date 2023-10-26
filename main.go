e09d069b243568a358b453e303d8051cbd835871
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-10-26 11:38:46 +0100

    proper cli args handling

--- main.go
+++ main.go
@@ -2,17 +2,54 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"replace-addrs/namada"
 )
 
-const sampleAddress = "atest1v4ehgw36x3prswzxggunzv6pxqmnvdj9xvcyzvpsggeyvs3cg9qnywf589qnwvfsg5erg3fkl09rg5"
-
 func main() {
-	addr, err := namada.ConvertAddress(sampleAddress)
-	if err != nil {
-		panic(err)
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
 	}
-	fmt.Println("old :", sampleAddress)
-	fmt.Println("new :", addr)
+}
+
+func run() error {
+	if len(os.Args) < 2 {
+		return dieUsage()
+	}
+
+	switch os.Args[1] {
+	case "convert":
+		if len(os.Args) != 3 {
+			return dieUsage()
+		}
+		return convert(os.Args[2])
+	}
+
+	return dieUsage()
+}
+
+func convert(oldAddr string) error {
+	newAddr, err := namada.ConvertAddress(oldAddr)
+	if err != nil {
+		return err
+	}
+	fmt.Println(newAddr)
+	return nil
+}
+
+func dieUsage() error {
+	usage(os.Stderr)
+	return fmt.Errorf("invalid cli arguments detected")
+}
+
+func usage(f *os.File) {
+	fmt.Fprintf(f, "usage: %s\n", os.Args[0])
+	fmt.Fprintln(f)
+
+	fmt.Fprintf(f, "\t# convert from the old to the new addr fmt,\n")
+	fmt.Fprintf(f, "\t# printing the new address to stdout\n")
+	fmt.Fprintf(f, "\tconvert <old-address>\n")
+	fmt.Fprintln(f)
 }
